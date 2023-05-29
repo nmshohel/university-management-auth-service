86b450435aa6220a9d803820412d243983cb0095
nmshohel <nmshohel1992@gmail.com>
2023-05-29 23:59:25 +0600

    setup eslint prettier

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
-const port = 3000
+const app: Application = express()
+// const port = 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-// for testing 
-app.get('/', (req:Request, res:Response) => {
+// for testing
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function boostrap() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log('Database Connection Successfully');
-        app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-          })
-
-    }
-    catch(err)
-    {
-        console.log("Database connection failed", err)
-
-    }
-    
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connection Successfully')
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Database connection failed', err)
   }
+}
 
-  boostrap();
+boostrap()
